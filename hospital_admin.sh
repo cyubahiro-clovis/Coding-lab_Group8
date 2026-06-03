4713c7b9d226c0f2f64fb9a67e0a96a1cba8abb2
ephrem-nziza <e.nziza@alustudent.com>
2026-06-03 05:27:24 -0700

    Commit 3

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-Initializing the system
-> source ./initialize_system.sh
-> source ./secure_data.sh
-> echo "Step 1: Initializing System..."
-> initialize_system
-> echo ""
-> echo "Step 2: Securing Data..."
-> secure_data
-> echo ""
-> echo "========================================="
-> echo " System Environment Secured"
-> echo " Date: $(date)"
-> echo "========================================="
+source ./initialize_system.sh
+source ./secure_data.sh
+echo "Step 1: Initializing System..."
+initialize_system
+echo ""
+echo "Step 2: Securing Data..."
+secure_data
+echo ""
+echo "========================================="
+echo " System Environment Secured"
+echo " Date: $(date)"
+echo "========================================="
