5b3ce3757ae50e87a456438ec1b5a9a83ffdbad5
digne <d.sugira@alustudent.com>
2026-06-03 06:19:26 -0700

    adding secure data function to the hospital_admin.sh function

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# hospital_admin.sh
-# Member 2 (Security Lead): secure_data()
-
 secure_data() {
-    echo "Securing the active_logs directory..."
+    echo "Securing the active_logs directory"
 
-    # Owner gets read + write + enter (x is needed to access a directory).
-    # Group and others get nothing.
     chmod 700 active_logs
 
     echo "New permissions:"
